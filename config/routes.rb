53808fb9422235d222f791dbb493e933ab3a1304
Samuel Ford <samuel.ford@icloud.com>
2015-03-01 10:13:32 -0500

    add app pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root 'posts#index'
+  
   get 'archive', to: 'posts#archive', as: 'archive'
   get 'unpublished', to: 'posts#unpublished', as: 'unpublished'
   get 'feed', to: 'posts#feed', as: 'feed'
+  
   resources :posts
   resources :images
+  
+  get 'game-friends', to: 'apps#game_friends'
+  get 'the-fox-and-the-grapes', to: 'apps#the_fox_and_the_grapes'
+  get 'rename-finder-items-action', to: 'apps#rename_finder_items_action'
 end
